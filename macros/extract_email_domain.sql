c53e964bbebaf2fcff8d8cee3ee7a49ff69129cf
Amit Tiwari <amit.tiwari@omaze.com>
2023-03-14 21:38:12 +0530

    is_personal_email & extract_email_domain fix.

--- macros/extract_email_domain.sql
+++ macros/extract_email_domain.sql
@@ -2,17 +2,26 @@
     {{ adapter.dispatch('extract_email_domain', 'census_utils') (email) }}
 {%- endmacro %}
 
-{% macro default__extract_email_domain(email) -%}
-    {%- set domain_extensions = {"com":".com", "coin":".co.in", "us":".us","uk":".uk","in":".in"} -%}
-    case 
-        when contains_substr(regexp_substr(lower(replace(rtrim({{ email }},'.'),' ','')), '@(.*)', 1, 1),'.') = true then regexp_substr(lower(replace(rtrim({{ email }},'.'),' ','')), '@(.*)', 1, 1)
-    {% for extension,newext in domain_extensions.items() %}
-        when regexp_contains(regexp_substr(lower(replace(rtrim({{ email }},'.'),' ','')), '@(.*)', 1, 1),'[^.]{{extension}}$') then regexp_replace(regexp_substr(lower(replace(rtrim({{ email }},'.'),' ','')), '@(.*)', 1, 1),'{{extension}}$','{{newext}}')
-    {% endfor %}
-    end
-{%- endmacro %}
+    {% macro default__extract_email_domain(email) -%}
+    
+        {% set sql_statement %}
+            select source_ext, target_ext from  {{ref('domain_extension_cleanup') }}
+        {% endset %}
 
-{%- macro snowflake__extract_email_domain(email) %}
-    regexp_substr(lower(replace(rtrim({{ email }},'.'),' ')), '@(.*)', 1, 1, 'e',1)
-{%- endmacro -%}
+        {%- set domain_extensions = dbt_utils.get_query_results_as_dict(sql_statement) -%}
+        
+        case 
+            when contains_substr(regexp_substr(lower(replace(rtrim({{ email }},'.'),' ','')), '@(.*)', 1, 1),'.') = true 
+                then regexp_substr(lower(replace(rtrim({{ email }},'.'),' ','')), '@(.*)', 1, 1)
+
+            {% for extension in domain_extensions['source_ext'] -%}
+                when regexp_contains(regexp_substr(lower(replace(rtrim({{ email }},'.'),' ','')), '@(.*)', 1, 1),'[^.]{{extension}}$') 
+                    then regexp_replace(regexp_substr(lower(replace(rtrim({{ email }},'.'),' ','')), '@(.*)', 1, 1),'{{extension}}$', '{{domain_extensions["target_ext"][loop.index -1 ]}}')
+            {% endfor %}
+        end
+    {%- endmacro %}
+
+    {%- macro snowflake__extract_email_domain(email) %}
+        regexp_substr(lower(replace(rtrim({{ email }},'.'),' ')), '@(.*)', 1, 1, 'e',1)
+    {%- endmacro -%}
 
---
+++ models/example/personal_email_test.sql
@@ -0,0 +1,8 @@
+{{ config(materialized="table") }}
+
+with email_addresses as (select * from {{ ref("census_email_addresses") }})
+
+select 
+    n.email_addresses,
+    {{ is_personal_email('n.email_addresses') }} as is_personal_email
+from email_addresses n
