14ea38cc38df57fe5ba54b1d80968ad58adbc8c3
Amit Tiwari <amit.tiwari@omaze.com>
2023-03-15 21:00:12 +0530

    Moved other models as well

--- integration_tests/models/test_extract_email_domain.sql
+++ integration_tests/models/test_extract_email_domain.sql
@@ -4,5 +4,5 @@ with email_addresses as (select * from {{ ref("census_email_addresses") }})
 
 select 
     n.email_addresses,
-    {{ extract_email_domain('n.email_addresses') }} as email_domain
+    {{ census_utils.extract_email_domain('n.email_addresses') }} as email_domain
 from email_addresses n
--- models/example/personal_email_test.sql
+++
@@ -1,8 +0,0 @@
-{{ config(materialized="table") }}
-
-with email_addresses as (select * from {{ ref("census_email_addresses") }})
-
-select 
-    n.email_addresses,
-    {{ is_personal_email('n.email_addresses') }} as is_personal_email
-from email_addresses n
