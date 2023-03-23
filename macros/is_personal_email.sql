2d1b1d0968725fcaa05740fab725e203823e8830
Amit Tiwari <amit.tiwari@omaze.com>
2023-03-23 21:10:06 +0530

    Updated the macro to use better code.

--- macros/is_personal_email.sql
+++ macros/is_personal_email.sql
@@ -4,11 +4,7 @@
 
     {% macro default__is_personal_email(email) -%}
     
-        {% set sql_statement %}
-            select array_agg(email_domains) email_domains from {{ref('free_email_providers') }}
-        {% endset %}
-
-        {%- set free_email_providers = dbt_utils.get_single_value(sql_statement) -%}
+        {%- set free_email_providers = dbt_utils.get_column_values(table=ref('free_email_providers'), column='email_domains') -%}
         
         case when regexp_substr(lower(replace(rtrim({{ email }},'.'),' ','')), '@(.*)', 1, 1) in unnest({{ free_email_providers }}) then true else false end
 
