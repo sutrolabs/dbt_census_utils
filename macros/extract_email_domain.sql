a5e483117e64eccaa336237bd7e8f2726e2d7339
Amit Tiwari <amit.tiwari@omaze.com>
2023-03-25 20:29:20 +0530

    Fixed the Snowflake Issues

--- macros/extract_email_domain.sql
+++ macros/extract_email_domain.sql
@@ -22,6 +22,19 @@
     {%- endmacro %}
 
     {%- macro snowflake__extract_email_domain(email) %}
-        regexp_substr(lower(replace(rtrim({{ email }},'.'),' ')), '@(.*)', 1, 1, 'e',1)
+        {% set sql_statement %}
+            select source_ext, target_ext from  {{ref('domain_extension_cleanup') }}
+        {% endset %}
+
+        {%- set domain_extensions = dbt_utils.get_query_results_as_dict(sql_statement) -%}
+
+        case when contains(regexp_substr(lower(replace(rtrim({{ email }},'.'),' ')), '@(.*)', 1, 1, 'e',1),'.') = true  
+                then regexp_substr(lower(replace(rtrim({{ email }},'.'),' ')), '@(.*)', 1, 1, 'e',1)
+
+            {% for extension in domain_extensions['SOURCE_EXT'] -%}
+                when regexp_instr(regexp_substr(lower(replace(rtrim({{ email }},'.'),' ','')), '@(.*)', 1, 1),'[^.]{{extension}}$') > 0
+                    then regexp_replace(regexp_substr(lower(replace(rtrim({{ email }},'.'),' ','')), '@(.*)', 1, 1,'e',1),'{{extension}}$', '{{domain_extensions["TARGET_EXT"][loop.index0 ]}}')
+            {% endfor %}
+        end
     {%- endmacro -%}
 
--- macros/is_personal_email.sql
+++ macros/is_personal_email.sql
@@ -9,3 +9,11 @@
         case when regexp_substr(lower(replace(rtrim({{ email }},'.'),' ','')), '@(.*)', 1, 1) in unnest({{ free_email_providers }}) then true else false end
 
     {%- endmacro %}
+
+    {% macro snowflake__is_personal_email(email) -%}
+    
+        {%- set free_email_providers = dbt_utils.get_column_values(table=ref('free_email_providers'), column='email_domains') -%}
+        
+        array_contains(regexp_substr(lower(replace(rtrim({{ email }},'.'),' ')), '@(.*)', 1, 1, 'e',1)::variant,{{free_email_providers}})
+
+    {%- endmacro %}
