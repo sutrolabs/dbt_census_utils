3c35fcfbc528ada5d6fba4227c166fe51d9378f4
Amit Tiwari <amit.tiwari@omaze.com>
2023-03-27 21:58:18 +0530

    Fixed the snowflake issue

--- macros/get_country_code.sql
+++ macros/get_country_code.sql
@@ -12,7 +12,23 @@
         
         case
         {% for cname in country_code_mapping['country_name'] -%}
-            when lower(country_name) = lower('{{cname | replace("'", "\\'")}}') then '{{country_code_mapping["country_code"][loop.index0 ]}}'
+            when lower(country_name) = lower('{{cname | replace("'", "\\'")}}') then '{{country_code_mapping["country_code"][loop.index0]}}'
+        {% endfor %}
+            when length(country_name) = 2 then upper(country_name)
+        end
+    {%- endmacro %}
+
+    {% macro snowflake__get_country_code(email) -%}
+    
+        {% set sql_statement %}
+            select country_name, country_code from  {{ref('country_codes') }}
+        {% endset %}
+
+        {%- set country_code_mapping = dbt_utils.get_query_results_as_dict(sql_statement) -%}
+        
+        case
+        {% for cname in country_code_mapping['COUNTRY_NAME'] -%}
+            when lower(country_name) = lower('{{cname | replace("'", "\\'")}}') then '{{country_code_mapping["COUNTRY_CODE"][loop.index0]}}'
         {% endfor %}
             when length(country_name) = 2 then upper(country_name)
         end
