6d9967baab1914b711869c6a1f98febd7e660b91
Stephen986 <stephenebrey@gmail.com>
2023-04-05 22:28:47 -0700

    Added defaults to use of var() is is_internal

--- macros/is_internal.sql
+++ macros/is_internal.sql
@@ -7,14 +7,14 @@
 
         case
             {% if email %}
-            {%- set exclusion = var("internal_email_column") ~ ' is not null' -%}
-            {%- set internal_email = dbt_utils.get_column_values(table=ref(var("internal_email_relation")), column=var("internal_email_column"), where=exclusion) -%}
-            when array_contains({{ census_utils.extract_email_domain(email) }}::variant, {{ dbt.array_construct(var("internal_domain")) }}) then true
+            {%- set exclusion = var("internal_email_column", 'email_address') ~ ' is not null' -%}
+            {%- set internal_email = dbt_utils.get_column_values(table=ref(var("internal_email_relation", 'census_internal_users')), column=var("internal_email_column", 'email_address'), where=exclusion) -%}
+            when array_contains({{ census_utils.extract_email_domain(email) }}::variant, {{ dbt.array_construct(var("internal_domain", 'getcensus.com')) }}) then true
             when array_contains({{ email }}::variant, {{internal_email}}) then true
             {%- endif %}
             {% if ip_address %}
-            {%- set exclusion = var("internal_ip_column") ~ ' is not null' -%}
-            {%- set internal_ip = dbt_utils.get_column_values(table=ref(var("internal_ip_relation")), column=var("internal_ip_column"), where=exclusion) -%}
+            {%- set exclusion = var("internal_ip_column", 'ip_address') ~ ' is not null' -%}
+            {%- set internal_ip = dbt_utils.get_column_values(table=ref(var("internal_ip_relation", 'census_internal_users')), column=var("internal_ip_column", 'ip_address'), where=exclusion) -%}
             when array_contains({{ ip_address }}::variant, {{internal_ip}}) then true
             {%- endif %}
             else false end
