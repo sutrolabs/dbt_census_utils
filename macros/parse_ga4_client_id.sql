59a845aaa7826148b68cc41861af5f6a7c1d1779
Amit Tiwari <amit.tiwari@omaze.com>
2023-05-02 21:14:56 +0530

    Fixed the GA4 Prefix Issue

--- macros/parse_ga4_client_id.sql
+++ macros/parse_ga4_client_id.sql
@@ -4,11 +4,11 @@
 
 {% macro default__parse_ga4_client_id(client_id,extract_value) -%}
     {%- if extract_value == 'unique_id' -%}
-        {{ split_part(string_text=client_id, delimiter_text="'.'", part_number=3) }}
+        {{ dbt.split_part(string_text=client_id, delimiter_text="'.'", part_number=3) }}
     {%- elif extract_value == 'timestamp' -%}
-        {{ split_part(string_text=client_id, delimiter_text="'.'", part_number=4) }}
+        {{ dbt.split_part(string_text=client_id, delimiter_text="'.'", part_number=4) }}
     {%- elif extract_value == 'client_id' -%}
-        replace({{ client_id }}, 'GA1.2.','')
+        {{ dbt.concat([dbt.split_part(string_text=client_id, delimiter_text="'.'", part_number=3), "'.'", dbt.split_part(string_text=client_id, delimiter_text="'.'", part_number=4) ])}}
     {% else %}
     {%- set error_message = '
     Warning: the `parse_ga4_client_id` macro only accepts unique_id, timestamp, or client_id as the extract_value. The {}.{} model triggered this warning. \
