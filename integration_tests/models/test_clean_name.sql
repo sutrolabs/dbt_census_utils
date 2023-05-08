c1f36c95c50bd4b87c187249e6ace37797553c11
Stephen986 <stephenebrey@gmail.com>
2023-05-08 14:40:00 -0700

    Add Google Clean Name

--- integration_tests/models/test_clean_name.sql
+++ integration_tests/models/test_clean_name.sql
@@ -3,5 +3,8 @@
 with names as (select * from {{ ref("census_names_to_clean") }})
 
 select 
-    {{ census_utils.clean_name(name, 'facebook') }} as name
+    {{ census_utils.clean_name(name, 'facebook') }} as facebook_name,
+    {{ census_utils.clean_name(name, 'google') }} as google_name,
+    {{ census_utils.clean_name(name) }} as default_name
+
 from names n
