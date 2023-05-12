b400cedbcea322c9bdc3d9f391137552374a1e66
Stephen986 <stephenebrey@gmail.com>
2023-05-12 11:22:19 -0700

    Fixed clean_name arg hardcoding

--- integration_tests/models/test_clean_name.sql
+++ integration_tests/models/test_clean_name.sql
@@ -3,8 +3,8 @@
 with names as (select * from {{ ref("census_names_to_clean") }})
 
 select 
-    {{ census_utils.clean_name(name, 'facebook') }} as facebook_name,
-    {{ census_utils.clean_name(name, 'google') }} as google_name,
-    {{ census_utils.clean_name(name) }} as default_name
+    {{ census_utils.clean_name('name_to_clean', 'facebook') }} as facebook_name,
+    {{ census_utils.clean_name('name_to_clean', 'google') }} as google_name,
+    {{ census_utils.clean_name('name_to_clean') }} as default_name
 
 from names n
