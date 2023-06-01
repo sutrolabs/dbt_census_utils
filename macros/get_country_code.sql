71db37300905c11ff6bdf69385eae5977c95530f
Amit Tiwari <amit.tiwari@omaze.com>
2023-06-01 14:06:18 +0530

    Code refactor for other macros to use subquery

--- macros/get_country_code.sql
+++ macros/get_country_code.sql
@@ -5,7 +5,7 @@
     {% macro default__get_country_code(country_name) -%}
     
         case when length({{ country_name }}) > 2 then (
-            select country_code 
+            select any_value(country_code) as country_code
             from {{ ref('census_utils_country_codes') }} 
             where lower(country_name) =  lower({{ country_name }})
         ) else upper({{ country_name }}) end 
