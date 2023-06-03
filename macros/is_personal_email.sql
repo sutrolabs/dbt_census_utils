a4750bd8f27a8d111263eecf06d4b824e22817d5
Amit Tiwari <amit.tiwari@omaze.com>
2023-06-03 20:59:15 +0530

    fixed the review comments

--- macros/is_personal_email.sql
+++ macros/is_personal_email.sql
@@ -6,7 +6,8 @@
     
         case 
             when {{ census_utils.extract_email_domain(email)}} is null and {{treat_null_as_false}} = false then null 
-            else coalesce((select any_value(true) from {{ ref('census_utils_free_email_providers') }} where {{ census_utils.extract_email_domain(email)}} = email_domains), false)
+            when (select {{ dbt.any_value(1) }} from {{ ref('census_utils_free_email_providers') }} where {{ census_utils.extract_email_domain(email)}} = email_domains) = 1 then true
+            else false
         end
 
     {%- endmacro %}
